e3d4f73cbd9988ac9ea574a08bf658a7eac7e186
Arthur Duarte <tuiduarte@gmail.com>
2025-03-25 20:28:51 -0400

    feat: add supabase auth session

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,35 @@
+import { useEffect, useState } from 'react'
 import './App.css'
 import Auth from './components/Auth'
-
+import { supabase } from './lib/supabaseClient'
+import { Session } from '@supabase/supabase-js'
+import Main from './components/Main'
 function App() {
+  const [session, setSession] = useState<Session | null>(null)
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session)
+    })
+
+    const { data: { subscription } } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session)
+    })
+    
+    return () => subscription.unsubscribe()
+  }, [])
+
+  if (!session) {
+    return (
+      <>
+        <Auth />
+      </>
+    )
+  }
 
   return (
     <>
-      <Auth />
+      <Main />
     </>
   )
 }
