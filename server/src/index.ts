6ac405c64579b226d72a6a264ef296819f299726
Arthur Duarte <tuiduarte@gmail.com>
2025-03-27 18:48:04 -0400

    feat: add routes and controllers for members

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,7 @@
 import 'dotenv/config';
 import express, { Request, Response } from 'express';
 import { supabaseAdmin } from './config/supabase';
+import memberRoutes from './routes/memberRoutes';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,6 +11,8 @@ app.use(express.json());
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: "hello world" });
 });
+
+app.use('/api/members', memberRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
