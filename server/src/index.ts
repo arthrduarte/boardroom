607ad4e1a9dbcf233c05da94d554324024449ded
Arthur Duarte <tuiduarte@gmail.com>
2025-03-27 19:13:09 -0400

    feat: add cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,11 +2,18 @@ import 'dotenv/config';
 import express, { Request, Response } from 'express';
 import { supabaseAdmin } from './config/supabase';
 import memberRoutes from './routes/memberRoutes';
+import cors from 'cors';  
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true,
+}));
 
 app.get('/', (req: Request, res: Response) => {
   res.json({ message: "hello world" });
