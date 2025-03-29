b8f3710fe442343c857d84bada319adfab7fda85
alexsousadev <alexandrosousapro@gmail.com>
2025-03-28 21:12:36 -0300

    Addition of the connection with Gemini/GPT. Addition of the base route for history and creation of the logic to generate responses from members based on user input, their descriptions, and background

--- server/src/config/gemini.ts
+++
@@ -1,8 +0,0 @@
-import {GoogleGenAI} from '@google/genai';
-
-const GEMINI_API_KEY = process.env.GEMINI_API_KEY;
-
-const gemini = new GoogleGenAI({apiKey: GEMINI_API_KEY});
-
-
-export default gemini;
--- server/src/routes/historyRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createHistory } from '../controllers/historyController';
-
-const router = express.Router();
-
-router.post("/", createHistory);
-
-export default router; 
