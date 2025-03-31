a43aac499c14414b64150dc7a0de98e7f3008825
Arthur Duarte <tuiduarte@gmail.com>
2025-03-31 10:18:07 -0400

    chore: add new types and fix packages

--- server/src/utils/types.ts
+++ server/src/utils/types.ts
@@ -13,5 +13,12 @@ export type History = {
     member_id: string;
     user_input: string;
     member_output: string;
+    historyParent_id: string;
+    created_at: string;
+}
+
+export type Picture = {
+    id: string;
+    url: string;
     created_at: string;
 }
